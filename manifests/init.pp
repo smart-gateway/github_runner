dc7182797ee26f1663a99cf0ff8d22575b9434ab
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-04 13:14:56 -0800

    add basic manifests

---
+++ manifests/init.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include github_runner
+class github_runner {
+}
---
+++ manifests/service.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include github_runner::service
+class github_runner::service {
+}
